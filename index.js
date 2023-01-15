03edb45015253a4d7971ddb270ae94622d2a82b9
ProtikHore <protik.hore@gmail.com>
2023-01-15 13:46:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const lodash = require('lodash');
+const people = require('./people');
+
+console.log(lodash.last(people.people));
+console.log(people.a);
+people.test();
---
+++ people.js
@@ -0,0 +1,13 @@
+const people = ['protik', 'kumer', 'hore'];
+
+const a = 5;
+
+function test() {
+    console.log('test');
+}
+
+module.exports = {
+    people,
+    a,
+    test,
+};
