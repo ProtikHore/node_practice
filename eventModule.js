c81577a79dffe0121543adbc7d07830971e45aa4
ProtikHore <protik.hore@gmail.com>
2023-01-15 23:27:24 +0600

    event

--- eventModule.js
+++ eventModule.js
@@ -3,9 +3,14 @@ const EventEmitter = require('events'); // it represent a class
 const emitter = new EventEmitter();
 
 // register a listner for bellRing event
-emitter.on('bellRing', () => {
-    console.log('need to run');
+emitter.on('bellRing', (text) => {
+    console.log(`need to run ${text}`);
+    console.log(text.text2);
 });
 
 // raise an event
-emitter.emit('bellRing');
+// emitter.emit('bellRing', 'this is perameter'); // 1st one is event, 2nd is paramerter
+emitter.emit('bellRing', {
+    text1: 'test',
+    text2: 'test2',
+}); // 1st one is event, 2nd is paramerter
